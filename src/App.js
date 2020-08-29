6bfdb73d1c145bd73e7e9ddc6d7e56402fd52eec
AsifRKabir <asifrkabir20@gmail.com>
2020-08-30 04:45:40 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+// import {Button} from '@material-ui/core';
 import './App.css';
+import News from './Components/News/News';
 
 function App() {
+
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    const url = "http://newsapi.org/v2/everything?q=bitcoin&from=2020-07-29&sortBy=publishedAt&apiKey=3a63e8d3667149988e0c53d08ef57baa";
+    fetch(url)
+    .then(res => res.json())
+    .then(data => {
+      setArticles(data.articles);
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        articles.map(article => <News article={article}></News>)
+      }
     </div>
   );
 }
---
+++ src/Components/News/News.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const News = ({article}) => {
+
+    const useStyles = makeStyles({
+        root: {
+          maxWidth: 345,
+        },
+      });
+
+    const classes = useStyles();
+
+    const { title, description, urlToImage  } = article;
+
+    return (
+        <Card className={classes.root}>
+            <CardActionArea>
+                <CardMedia
+                    component="img"
+                    alt="Contemplative Reptile"
+                    height="140"
+                    image={urlToImage}
+                    title="Contemplative Reptile"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                        {title}
+          </Typography>
+                    <Typography variant="body2" color="textSecondary" component="p">
+                        {description}
+          </Typography>
+                </CardContent>
+            </CardActionArea>
+            <CardActions>
+                <Button size="small" color="primary">
+                    Share
+        </Button>
+                <Button size="small" color="primary">
+                    Learn More
+        </Button>
+            </CardActions>
+        </Card>
+    );
+};
+
+export default News;
